1d3bbc519d77cf80369f964cd283de73a302917d
SergeyZinkovich <my_agent-1@mail.ru>
2020-05-10 21:02:46 +1000

    Add mlp neural network

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import train
 import theme_counter
+import neural_network
 
 DATASET_FILENAME = 'without_mess.csv'
 
-print("=========WITHOUT UPDATE=========\n")
-for size in range(10, 200, 10):
-    train.train(DATASET_FILENAME, size, fasttext=True)
-    theme_counter.count_theme_dict(train.load_trained_model(), DATASET_FILENAME)
-    print(size, " ")
-    print(theme_counter.test(train.load_trained_model(), DATASET_FILENAME), "\n")
 
-print("=========WITH UPDATE=========\n")
-for size in range(10, 200, 10):
-    train.train(DATASET_FILENAME, size)
-    train.update(train.load_trained_model(), 'ruscorpora_mean_hs.model', True)
-    theme_counter.count_theme_dict(train.load_updated_model(), DATASET_FILENAME)
-    print(size, " ")
-    print(theme_counter.test(train.load_updated_model(), DATASET_FILENAME), "\n")
+def test_nn():
+    dct, tfidf = theme_counter.load_tfidf()
+    w2v_model = train.load_trained_model()
+    x, y, theme_dict = neural_network.prepare_data(DATASET_FILENAME, w2v_model, dct, tfidf)
+    # x, y, theme_dict = neural_network.load_prepared_data()
+    neural_network.train(w2v_model.vector_size, x, y, len(theme_dict))
+    neural_network.test(x, y)
 
+
+def test_w2v():
+    print("=========WITHOUT UPDATE=========\n")
+    for size in range(10, 200, 10):
+        train.train(DATASET_FILENAME, size, fasttext=False)
+        theme_counter.count_theme_dict(train.load_trained_model(), DATASET_FILENAME)
+        print(size, " ")
+        print(theme_counter.test(train.load_trained_model(), DATASET_FILENAME), "\n")
+
+    print("=========WITH UPDATE=========\n")
+    for size in range(10, 200, 10):
+        train.train(DATASET_FILENAME, size)
+        train.update(train.load_trained_model(), 'ruscorpora_mean_hs.model', True)
+        theme_counter.count_theme_dict(train.load_updated_model(), DATASET_FILENAME)
+        print(size, " ")
+        print(theme_counter.test(train.load_updated_model(), DATASET_FILENAME), "\n")
+
+
+test_nn()
+test_w2v()
---
+++ neural_network.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pandas as pd
+import keras
+import pickle
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Activation, Input
+from preprocess_udpipe import done_text
+
+
+TRAIN_TEST_SPLIT = 0.8
+NN_MODEL_FILENAME = 'nn_model/model.h5'
+X_ARRAY_FILENAME = 'nn_model/x_array.npy'
+Y_ARRAY_FILENAME = 'nn_model/y_array.npy'
+THEME_DICT_FILENAME = 'nn_model/theme_dict.pkl'
+
+
+def get_data(dataset_filename):
+    data = pd.read_csv(dataset_filename, encoding='utf-8', delimiter=';', header=None, names=['id', 'text'])
+    return data.sample(frac=1).reset_index(drop=True)
+
+
+def prepare_data(dataset_filename, w2v_model, dct, tfidf):
+    data = get_data(dataset_filename)
+    theme_dict = {}
+    x = []
+    y = []
+    j = -1
+
+    for i in data.values:
+        if not i[0] in theme_dict:
+            j += 1
+            theme_dict[i[0]] = j
+        words = done_text(str(i[1]))
+        vec = 0
+        for w in words:
+            if w not in w2v_model.wv:
+                continue
+            v = w2v_model.wv.get_vector(w)
+            if vec == 0:
+                vec = [v * tfidf.idfs[dct.token2id[w]], 1]
+            else:
+                vec = [vec[0] + v * tfidf.idfs[dct.token2id[w]], vec[1] + 1]
+        vec = vec[0] / vec[1]
+        x.append(vec)
+        y.append(theme_dict[i[0]])
+
+    x = np.array(x)
+    y = np.array(y)
+    np.save(X_ARRAY_FILENAME, x)
+    np.save(Y_ARRAY_FILENAME, y)
+    with open(THEME_DICT_FILENAME, 'wb') as f:
+        pickle.dump(theme_dict, f, pickle.HIGHEST_PROTOCOL)
+    return x, y, theme_dict
+
+
+def load_prepared_data():
+    x = np.load(X_ARRAY_FILENAME)
+    y = np.load(Y_ARRAY_FILENAME)
+    with open(THEME_DICT_FILENAME, 'rb') as f:
+        theme_dict = pickle.load(f)
+    return x, y, theme_dict
+
+
+def train(w2v_model_vector_size, x, y, theme_dict_len):
+    train_size = int(len(x) * TRAIN_TEST_SPLIT)
+    x_train = x[:train_size]
+    x_test = x[train_size:]
+    y_train = y[:train_size]
+    y_test = y[train_size:]
+
+    model = Sequential()
+    model.add(Dense(256, input_shape=(w2v_model_vector_size,)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(theme_dict_len))
+    model.add(Activation('softmax'))
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train, batch_size=128, epochs=1000, validation_data=(x_test, y_test), class_weight='auto')
+    model.save(NN_MODEL_FILENAME)
+
+
+def test(x, y):
+    train_size = int(len(x) * TRAIN_TEST_SPLIT)
+    x_train = x[:train_size]
+    x_test = x[train_size:]
+    y_train = y[:train_size]
+    y_test = y[train_size:]
+
+    model = keras.models.load_model(NN_MODEL_FILENAME)
+    score = model.evaluate(x_test, y_test, batch_size=128, verbose=1)
+    print('test accuracy: ', score[1])
+
+    model = keras.models.load_model(NN_MODEL_FILENAME)
+    score = model.evaluate(x, y, batch_size=128, verbose=1)
+    print('total accuracy: ', score[1])
+
