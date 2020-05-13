6a7529dc9c2dc890dfbb10d2c1258766de4a05c0
SergeyZinkovich <my_agent-1@mail.ru>
2020-05-13 15:27:26 +1000

    Add lstm with 2d input

--- neural_network.py
+++ neural_network.py
@@ -4,7 +4,7 @@ import keras
 import pickle
 import matplotlib.pyplot as plt
 from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Activation, Input
+from keras.layers import Dense, Dropout, Activation
 from preprocess_udpipe import done_text
 
 
@@ -13,6 +13,7 @@ NN_MODEL_FILENAME = 'nn_model/model.h5'
 X_ARRAY_FILENAME = 'nn_model/x_array.npy'
 Y_ARRAY_FILENAME = 'nn_model/y_array.npy'
 THEME_DICT_FILENAME = 'nn_model/theme_dict.pkl'
+META_DICT_FILENAME = 'nn_model/meta_dict.pkl'
 
 
 def get_data(dataset_filename):
@@ -26,32 +27,41 @@ def prepare_data(dataset_filename, w2v_model, dct, tfidf):
     x = []
     y = []
     j = -1
+    max_words = 0
 
     for i in data.values:
         if not i[0] in theme_dict:
             j += 1
             theme_dict[i[0]] = j
         words = done_text(str(i[1]))
-        vec = 0
+        vec = []
         for w in words:
             if w not in w2v_model.wv:
                 continue
-            v = w2v_model.wv.get_vector(w)
-            if vec == 0:
-                vec = [v * tfidf.idfs[dct.token2id[w]], 1]
-            else:
-                vec = [vec[0] + v * tfidf.idfs[dct.token2id[w]], vec[1] + 1]
-        vec = vec[0] / vec[1]
+            vec.append(w2v_model.wv.get_vector(w))
+            if len(vec) > 1000:
+                break
+        if len(vec) > max_words:
+            max_words = len(vec)
         x.append(vec)
         y.append(theme_dict[i[0]])
-
+    for i in range(len(x)):
+        x[i] = np.pad(x[i], [(0, max_words - len(x[i])), (0, 0)], 'constant', constant_values=.0)
     x = np.array(x)
     y = np.array(y)
+
+    meta_dict = {
+        'max_words': max_words
+    }
+
     np.save(X_ARRAY_FILENAME, x)
     np.save(Y_ARRAY_FILENAME, y)
     with open(THEME_DICT_FILENAME, 'wb') as f:
         pickle.dump(theme_dict, f, pickle.HIGHEST_PROTOCOL)
-    return x, y, theme_dict
+    with open(META_DICT_FILENAME, 'wb') as f:
+        pickle.dump(meta_dict, f, pickle.HIGHEST_PROTOCOL)
+
+    return x, y, theme_dict, max_words
 
 
 def load_prepared_data():
@@ -59,10 +69,12 @@ def load_prepared_data():
     y = np.load(Y_ARRAY_FILENAME)
     with open(THEME_DICT_FILENAME, 'rb') as f:
         theme_dict = pickle.load(f)
-    return x, y, theme_dict
+    with open(META_DICT_FILENAME, 'rb') as f:
+        meta_dict = pickle.load(f)
+    return x, y, theme_dict, meta_dict['max_words']
 
 
-def train(w2v_model_vector_size, x, y, theme_dict_len):
+def train(w2v_model_vector_size, x, y, max_words, theme_dict_len):
     train_size = int(len(x) * TRAIN_TEST_SPLIT)
     x_train = x[:train_size]
     x_test = x[train_size:]
@@ -70,9 +82,9 @@ def train(w2v_model_vector_size, x, y, theme_dict_len):
     y_test = y[train_size:]
 
     model = Sequential()
-    model.add(Dense(256, input_shape=(w2v_model_vector_size,)))
+    model.add(keras.layers.LSTM(100, input_shape=(max_words, w2v_model_vector_size), return_sequences=True, dropout=0.2, recurrent_dropout=0.2))
     model.add(Activation('relu'))
-    model.add(Dropout(0.5))
+    model.add(keras.layers.Flatten())
     model.add(Dense(theme_dict_len))
     model.add(Activation('softmax'))
     model.compile(loss='sparse_categorical_crossentropy',
@@ -80,7 +92,7 @@ def train(w2v_model_vector_size, x, y, theme_dict_len):
                   metrics=['accuracy'])
     print(model.summary())
 
-    train_history = model.fit(x_train, y_train, batch_size=128, epochs=10, validation_data=(x_test, y_test), class_weight='auto')
+    train_history = model.fit(x_train, y_train, batch_size=128, epochs=2000, validation_data=(x_test, y_test), class_weight='auto')
     show_history_plot(train_history)
     model.save(NN_MODEL_FILENAME)
 
